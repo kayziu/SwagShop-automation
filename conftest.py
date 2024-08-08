5e43fd77f7062b441d46719519e3036c799a756f
IwoUK <106241738+IwoUK@users.noreply.github.com>
2024-08-08 18:33:09 +0200

    fixing scope for the setup_browser

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from POM.login_page import LoginPage
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def setup_browser(page):
     # browser = playwright.chromium.launch(headless=False)
     # context = browser.new_context()
@@ -13,7 +13,7 @@ def setup_browser(page):
     yield page
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def logging_in(setup_browser) -> None:
     username = 'standard_user'
     passwd = 'secret_sauce'
